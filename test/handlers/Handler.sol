f74f33caf8e7be9c8f178b39c82f2c3fd2567048
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2023-08-15 10:00:57 +0100

    create address set library

--- test/handlers/Handler.sol
+++ test/handlers/Handler.sol
@@ -10,6 +10,34 @@ import {WETH9} from "src/WETH9.sol";
 import {Test} from "forge-std/Test.sol";
 
 
+struct AddressSet {
+    address[] addrs;
+    mapping(address => bool) saved;
+}
+
+library LibAddressSet {
+    function add(AddressSet storage s, address addr) internal {
+        if (!s.saved[addr]) {
+            s.addrs.push(addr);
+            s.saved[addr] = true;
+        }
+    }
+
+    function contains(
+        AddressSet storage s, 
+        address addr
+    ) internal view returns (bool) {
+        return s.saved[addr];
+    }
+
+    function count(
+        AddressSet storage s
+    ) internal view returns (uint256) {
+        return s.addrs.length;
+    }
+}
+
+
 contract Handler is Test{
     using LibAddressSet for AddressSet;
 
